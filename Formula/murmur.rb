e7fd5f6ba66b87884416820bb3604acf538b8020
Kieran Klukas <me@dunkirk.sh>
2025-11-14 21:47:38 -0500

    feat: bump murmur to v0.1.1

--- Formula/murmur.rb
+++ Formula/murmur.rb
@@ -2,13 +2,17 @@ class Murmur < Formula
   desc "Job-based Whisper transcription server for macOS with Neural Engine acceleration"
   homepage "https://github.com/taciturnaxolotl/murmur"
   url "https://github.com/taciturnaxolotl/murmur.git",
-      tag:      "v0.1.0",
-      revision: "ac9e8ae71da91b70faa0d116f46bba83d589606b"
+      tag:      "v0.1.1",
+      revision: "9a327406068d12c646cc97cf38f88dbc4bb4d94f"
   license "AGPL-3.0"
   head "https://github.com/taciturnaxolotl/murmur.git", branch: "main"
 
   depends_on xcode: ["14.0", :build]
   depends_on arch: :arm64
+  
+  # To update to a new version, run:
+  # 1. Find the latest tag: curl -s https://api.github.com/repos/taciturnaxolotl/murmur/releases/latest | grep 'tag_name' | cut -d'"' -f4
+  # 2. Get commit hash: git ls-remote --tags https://github.com/taciturnaxolotl/murmur.git refs/tags/VERSION | cut -f1
   depends_on macos: :ventura
 
   def install
